9496e2359e5f19a438bb8c3dc5c9bf29234db21b
0niv001 <nirvesh_99@proton.me>
2025-01-29 13:38:13 +0000

    Add login to home and redirect to login page

--- src/main/java/com/social/socialapp/ui/MainView.java
+++ src/main/java/com/social/socialapp/ui/MainView.java
@@ -1,7 +1,15 @@
 package com.social.socialapp.ui;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
@@ -9,8 +17,17 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 @AnonymousAllowed
 public class MainView extends VerticalLayout {
     public MainView() {
-        add(
-                new H1("TEST")
-        );
+        H1 title = new H1("SocialHub");
+
+        // Login Button
+        Button signInButton = new Button();
+        signInButton.setText("Log In");
+        signInButton.addClickListener(event -> signInButton.getUI().ifPresent(ui -> ui.navigate("login")));
+        HorizontalLayout navBar = new HorizontalLayout(title, signInButton);
+        navBar.setWidthFull();
+        navBar.setJustifyContentMode(JustifyContentMode.BETWEEN);
+
+
+        add(navBar);
     }
 }
