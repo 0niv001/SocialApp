10d042d5af847e2d2b33bef2ce27bb4b334456d5
0niv001 <nirvesh_99@proton.me>
2025-01-29 11:53:00 +0000

    Create login functionality

--- src/main/java/com/social/socialapp/ui/LoginView.java
+++ src/main/java/com/social/socialapp/ui/LoginView.java
@@ -1,4 +1,15 @@
 package com.social.socialapp.ui;
 
-public class LoginView {
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinService;
+
+@Route("login")
+public class LoginView extends Composite<LoginOverlay> {
+    public LoginView() {
+        getContent().setOpened(true);
+        getContent().setAction("login");
+
+    }
 }
