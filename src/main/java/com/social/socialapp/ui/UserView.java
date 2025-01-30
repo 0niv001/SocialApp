e584663463c4f2c6c5e7e33bb7ad96eeb5983901
0niv001 <nirvesh_99@proton.me>
2025-01-30 13:30:50 +0000

    login redirect error

--- src/main/java/com/social/socialapp/ui/UserView.java
+++ src/main/java/com/social/socialapp/ui/UserView.java
@@ -3,14 +3,12 @@ package com.social.socialapp.ui;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
-import org.springframework.stereotype.Component;
 
-@Route("/user")
-@RolesAllowed("USER")
-@Component
+@Route("user")
+@PermitAll
 public class UserView extends VerticalLayout {
-
 
     public UserView() {
         add(new H1("Welcome User"));
