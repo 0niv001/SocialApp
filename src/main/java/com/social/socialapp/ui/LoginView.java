fe205a13cbfd726ab5a82794b24aa4da41148895
0niv001 <nirvesh_99@proton.me>
2025-02-14 08:57:22 +0000

    New LoginView

--- src/main/java/com/social/socialapp/ui/LoginView.java
+++ src/main/java/com/social/socialapp/ui/LoginView.java
@@ -4,14 +4,13 @@ import com.social.socialapp.services.UserService;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.PreserveOnRefresh;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
@@ -27,6 +26,59 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @AnonymousAllowed
 @PreserveOnRefresh
 public class LoginView extends VerticalLayout {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    public LoginView(UserService userService) {
+        this.userService = userService;
+        LoginOverlay loginOverlay = new LoginOverlay();
+        loginOverlay.setTitle("Creator Spot");
+        loginOverlay.setDescription("Work on cool projects with cool people");
+        loginOverlay.setForgotPasswordButtonVisible(false);
+        loginOverlay.addLoginListener(e -> {
+            System.out.println("Login event triggered");
+            boolean authenticated = authenticate(e.getUsername(), e.getPassword());
+           if (authenticated) {
+               loginOverlay.close();
+               UI.getCurrent().navigate(UserView.class);
+               //getUI().ifPresent(ui -> ui.navigate(UserView.class));
+           }
+           else {
+               loginOverlay.setError(true);
+               loginOverlay.showErrorMessage("Login Failed", "Username or password is incorrect");
+           }
+        });
+        loginOverlay.setOpened(true);
+
+        Anchor signUpLink = new Anchor("signup", "Don't have an account? Sign Up");
+        signUpLink.getStyle().set("text-align", "center").set("margin-top", "10px");
+
+        add(loginOverlay, signUpLink);
+    }
+
+
+
+    private boolean authenticate(String username, String password) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return authentication.isAuthenticated();
+        } catch (AuthenticationException e) {
+            return false; // Authentication failed
+        }
+    }
+
+}
+
+/*
+public class LoginView extends VerticalLayout {
 
     @Autowired
     private UserService userService;
@@ -38,6 +90,11 @@ public class LoginView extends VerticalLayout {
     public LoginView(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.authenticationManager = authenticationManager;
+
+        setAlignItems(Alignment.CENTER);
+        setSpacing(true);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setSizeFull();
         // Username Field
         TextField username = new TextField("Username");
         username.setPlaceholder("Username");
@@ -50,7 +107,7 @@ public class LoginView extends VerticalLayout {
 
         //Login Button
         Button login = new Button("Login");
-        login.addClickListener(e -> {
+        login.addClickListener(_ -> {
             String username_check = username.getValue();
             String password_check = password.getValue();
             if (authenticate(username_check, password_check)) {
@@ -64,22 +121,14 @@ public class LoginView extends VerticalLayout {
         });
         login.addClickShortcut(Key.ENTER);
 
-        Button githubButton = new Button("GitHub",VaadinIcon.LOCK.create());
-
-        //TODO: Add GitHub OAuth2 - Should I store this in the DB?
-        /*
-        githubButton.addClickListener(e -> {
-            break;
-        })
-         */
-
+        //Button githubButton = new Button("GitHub", VaadinIcon.LOCK.create());
 
         //Signup redirect
         H3 noAccount = new H3("Don't have an account? Click the button to sign up");
         Button signUpRedirect = new Button("Sign Up");
-        signUpRedirect.addClickListener(e -> {UI.getCurrent().navigate(UserView.class);});
+        signUpRedirect.addClickListener(_ -> UI.getCurrent().navigate(SignupView.class));
 
-        FormLayout form = new FormLayout(username, password, login, githubButton, noAccount, signUpRedirect);
+        VerticalLayout form = new VerticalLayout(username, password, login, noAccount, signUpRedirect);
         add(form);
     }
 
@@ -93,10 +142,12 @@ public class LoginView extends VerticalLayout {
 
             return authentication.isAuthenticated();
         } catch (AuthenticationException e) {
-           return false; // Authentication failed
+            return false; // Authentication failed
         }
     }
 
     //TODO: Work with HTTP Session to keep user signed in as they use the site
 
 }
+
+ */
